cbaeaf3bbc708f4d3f716db1a7a68231e2c6daba
Matthew Westrik <m@ttwestrik.com>
2018-07-19 03:23:51 -0400

    Output only black

--- app/Main.hs
+++ app/Main.hs
@@ -18,7 +18,4 @@ generateImg :: DynamicImage
 generateImg = ImageRGB8 (generateImage originalFnc 1200 1200)
 
 originalFnc :: Int -> Int -> PixelRGB8
-originalFnc x y =
-  let (q, r) = x `quotRem` max 10 y
-      s      = fromIntegral . min 0xff
-  in PixelRGB8 (s q) (s r) (s (q + r + 30))
+originalFnc x y = PixelRGB8 0 0 0
